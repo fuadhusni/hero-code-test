7e9bcde3e2ff281bb4bcb8a36f2f19f1fba78d2f
Fuad Husni <fuadhusni@gmail.com>
2020-11-29 06:41:35 +0100

    add search page & activities controller

---
+++ Controllers/ActivitiesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hero_code_test.Controllers
+{
+	public class ActivitiesController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Search()
+		{
+			return View("Search");
+		}
+	}
+}
